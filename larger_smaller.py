92b2f5f1289265ae5fd7f0b7d4e15b1c5c076273
suhaibmukhtar <suhaibmukhtar2@gmail.com>
2024-10-04 11:00:30 +0530

    uploading the larger and smaller of two numbers

---
+++ larger_smaller.py
@@ -0,0 +1,11 @@
+#larger of two numbers
+num1=int(input("Enter the number 1::"))
+num2=int(input("Enter the number 2::"))
+if num1>num2:
+    print(f"{num1} is greater than {num2}")
+else:
+    print(f"{num2} is greater than {num1}")
+if num1>num2:
+    print(f"{num2} is Smaller than {num1}")
+else:
+    print(f"{num1} is Smaller than {num2}")
