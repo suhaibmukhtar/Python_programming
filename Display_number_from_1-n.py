69e02168570c56113bfe05f01632370986160649
suhaibmukhtar <suhaibmukhtar2@gmail.com>
2024-10-04 11:08:19 +0530

    Basic-Loops_programs

---
+++ Display_number_from_1-n.py
@@ -0,0 +1,5 @@
+num=int(input("enter the number upto which you want to display numbers::"))
+i=0
+while(i<=num):
+    print(f"{i}",end=" ")
+    i+=1
---
+++ Reverse_sequence_integers_2_diff.py
@@ -0,0 +1,5 @@
+#Program to print numbers in reverse order with a difference of 2
+num=int(input("Enter the last number from which you want reverse the numbers::"))
+while (num>=0):
+    print(f"{num}",end=" ")
+    num-=2
